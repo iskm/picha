5656952c81ce423065f3f8e6a71d94deb79d74bc
Ibrahim Mkusa <ibrahimmkusa@gmail.com>
2024-11-16 22:34:32 -0500

    lets make it homie

--- scripts/initial.sh
+++ scripts/initial.sh
@@ -7,6 +7,12 @@ IFS=$'\n\t'
 case $ID in
   "debian" )
     sudo apt update -y && sudo apt upgrade -y
+    sudo apt install vim git curl tmux stow ranger
+    cd /home/admin/
+    git clone git@github.com:iskm/dots.git
+    mv .bashrc .bashrc.bak; mv .bash_profile .bash_profile.bak
+    cd dots
+    stow bash bin git shellenv tmux vim ranger
     ;;
   "rhel" )
     sudo dnf update -y
